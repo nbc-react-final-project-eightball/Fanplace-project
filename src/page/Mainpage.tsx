6d59bd8e9c81575192a07b5b2d0aeff6ad75e921
STINGRAY001 <146930405+STINGRAY001@users.noreply.github.com>
2024-01-10 19:37:02 +0900

    Revert "Feature/main page"

--- src/page/Mainpage.tsx
+++ src/page/Mainpage.tsx
@@ -1,38 +1,7 @@
-import React from 'react';
-import * as S from '../styledComponent/styledMain/StMain';
-import MainBottomCarousel from '../components/Main/MainBottomCarousel';
-import MainBttomAlbum from '../components/Main/MainBttomAlbum';
-
+import React from "react";
+import * as S from "../styledComponent/styledMain/StMain";
 const Mainpage = () => {
-  return (
-    <>
-      <div
-        style={{
-          width: '90%',
-          height: '170vh',
-          margin: 'auto',
-          justifyContent: 'center',
-          overflow: 'hidden',
-        }}
-      >
-        <MainBottomCarousel />
-        <MainBottomCarousel />
-        <MainBttomAlbum />
-        {/* TODO: 나중에 백그라운드 이미지 변경 해야함 */}
-        <div
-          style={{
-            backgroundImage: `url('img/bg1.jpg')`,
-            backgroundSize: '320',
-            backgroundRepeat: 'no-repeat',
-            backgroundAttachment: 'fixed',
-            backgroundPosition: 'right 25% center',
-            height: '20vh',
-            width: '100%',
-          }}
-        />
-      </div>
-    </>
-  );
+  return <div>Main</div>;
 };
 
 export default Mainpage;
