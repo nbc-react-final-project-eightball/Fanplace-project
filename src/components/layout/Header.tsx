db28a99ac0355c76061462179b815cfcc2415336
Kimcowtooth <toaanf111@gmail.com>
2024-01-11 15:45:39 +0900

    Header수정 #20

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,19 @@
 import { useEffect, useState } from 'react';
-import * as S from '../../styledComponent/styledHeader/StHeader';
-import BasicHeader from './BasicHeader';
-import ChangeHeader from './ChangeHeader';
+import * as S from '../../styledComponent/styledLayout/StHeader';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const homeClickHandler = () => {
+    navigate('/');
+  };
+  const loginHandler = () => {
+    navigate('/');
+  };
+  const wishListHandler = () => {
+    navigate('/');
+  };
   const [scrollPostion, setScrollPoistion] = useState<number>(0);
   const updateScroll = () => {
     setScrollPoistion(window.scrollY || document.documentElement.scrollTop);
@@ -13,8 +23,19 @@ const Header = () => {
   });
 
   return (
-    <S.HeaderContainer>
-      {scrollPostion < 150 ? <BasicHeader /> : <ChangeHeader />};
+    <S.HeaderContainer color={scrollPostion < 150 ? 'transparent' : 'black'}>
+      <S.Wrapper>
+        <S.Header>
+          <S.BrandLogo onClick={homeClickHandler}>FancyPlace</S.BrandLogo>
+        </S.Header>
+        <S.Header>
+          <S.HeaderButton>
+            <button onClick={loginHandler}>로그인</button>
+            <button onClick={wishListHandler}>장바구니</button>
+            <button>KoR \</button>
+          </S.HeaderButton>
+        </S.Header>
+      </S.Wrapper>
     </S.HeaderContainer>
   );
 };
--- src/styledComponent/styledLayout/StHeader.tsx
+++ src/styledComponent/styledLayout/StHeader.tsx
@@ -1,12 +1,67 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
+  background: ${({ color }) => color};
+  color: ${({ color }) => (color === 'transparent' ? 'black' : 'white')};
   display: flex;
   justify-content: center;
   position: fixed;
   z-index: 1000;
   width: 100%;
   height: 90px;
-
+  border: 2px solid white;
   box-shadow: var(--box-shadow);
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  > div {
+    cursor: pointer;
+  }
+  .logo {
+    width: 200px;
+  }
+  button {
+    background: transparent;
+    padding: 3px;
+    border: none;
+    padding: 10px;
+    font-size: 20px;
+    text-shadow: 2px 4px 2px gray;
+    font-weight: 700;
+    cursor: pointer;
+  }
+`;
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+  img {
+    width: 70%;
+    height: 40%;
+    object-fit: cover;
+    cursor: pointer;
+  }
+`;
+export const BrandLogo = styled.div`
+  margin-top: 50px;
+  padding-left: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+`;
+export const HeaderButton = styled.div`
+  margin-top: 50px;
+  padding-right: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5rem;
 `;
