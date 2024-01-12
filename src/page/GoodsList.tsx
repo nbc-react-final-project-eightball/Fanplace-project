ed009944ed5fe46f248a9a7d47a240fe8b3d4a98
JunHo <wnsgh2098@gmail.com>
2024-01-12 14:28:52 +0900

    [feature/GoodsList] 2중 필터기능 추가 상품종류 추가 Layout 스타일 vh 수정  #19

--- src/page/GoodsList.tsx
+++ src/page/GoodsList.tsx
@@ -1,77 +1,239 @@
 import React from 'react';
 import * as S from '../styledComponent/styledGoodsList/StGoodsList';
-import { info } from 'console';
+
 const GoodsList = () => {
   const ProducList = [
     {
-      id:"후드티",
+      category: 'CD',
       info: '[2/13출시]',
-      img: 'img/ProductCardImg1.jpg',
-      title: '후드티',
-      price: '50000원',
+      img: 'img/ProductCardImg/YO1.jpg',
+      artist: 'YOASOBI',
+      title: 'YOASOBI - 07 YOASOBI THE BOOK 3 / ASIA TOUR 2023-2024 OFFICIAL',
+      price: '46000원',
       teg: 'img/new.svg',
     },
     {
-      id:"모자",
+      category: 'CD',
       info: '[2/13출시]',
-      img: 'img/ProductCardImg1.jpg',
-      title: '모자',
-      price: '50000원',
+      img: 'img/ProductCardImg/YO2.jpg',
+      artist: 'YOASOBI',
+      title: 'YOASOBI - 13 アイドル / ASIA TOUR 2023-2024 OFFICIAL MD',
+      price: '30000원',
       teg: 'img/new.svg',
     },
     {
-      id:"티셔츠",
-      info: '[2/13출시]',
-      img: 'img/ProductCardImg1.jpg',
-      title: '티셔츠',
-      price: '50000원',
+      category: '티셔츠',
+      info: '[CONNECTION]',
+      img: 'img/ProductCardImg/Boa1.png',
+      artist: '보아',
+      title: 'BoA',
+      price: '41000원',
       teg: 'img/new.svg',
     },
-
+    {
+      category: 'CD',
+      info: '[2/13출시]',
+      artist: '있지',
+      img: 'img/ProductCardImg/있지1.jpg',
+      title: '있지 - BORN TO BE (SPECIAL EDITION) (Mr. Vampire Ver.)',
+      price: '42000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: '잠옷',
+      info: '[2/13출시]',
+      artist: '에스파',
+      img: 'img/ProductCardImg/ESPA.png',
+      title: 'PAJAMAS [NINGNING Ver.]',
+      price: '32000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: 'CD',
+      info: '[2/13출시]',
+      artist: '칸나',
+      img: 'img/ProductCardImg/칸나2.jpg',
+      title: '최종화 (The Last Flower)',
+      price: '55000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: 'CD',
+      info: '[2/13출시]',
+      artist: '칸나',
+      img: 'img/ProductCardImg/칸나1.jpg',
+      title: 'ADDICT!ON',
+      price: '40000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: '머그컵',
+      info: '[2/13출시]',
+      artist: '아이즈원',
+      img: 'img/ProductCardImg/아이즈원1.jpg',
+      title: '아이즈원 - 보틀 / HEART*IZ POP-UP STORE',
+      price: '8000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: '티셔츠',
+      info: '[2/13출시]',
+      artist: '아이즈원',
+      img: 'img/ProductCardImg/아이즈원2.jpg',
+      title: '아이즈원 - 02 티셔츠 / 2020 ONEIRIC THEATER',
+      price: '38000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: '티셔츠',
+      info: '[2/13출시]',
+      artist: '트와이스',
+      img: 'img/ProductCardImg/트와이스1.jpg',
+      title: '트와이스 - 23 텀블러 / 2019 ONCE HALLOWEEN 2',
+      price: '25000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: '목걸이',
+      info: '[2/13출시]',
+      artist: '트와이스',
+      img: 'img/ProductCardImg/트와이스2.jpg',
+      title: '트와이스 - 27 목걸이 / 4TH WORLD TOUR Ⅲ 2ND MD',
+      price: '25000원',
+      teg: 'img/new.svg',
+    },
+    {
+      category: '핸드폰케이스',
+      info: '[2/13출시]',
+      artist: '트와이스',
+      img: 'img/ProductCardImg/트와이스3.jpg',
+      title: '트와이스 - 04 폰케이스 / 2019 ONCE HALLOWEEN 2',
+      price: '38000원',
+      teg: 'img/new.svg',
+    },
   ];
-  const [filter, setFilter] = React.useState<null|String>('');
-  const handleFilterTeb = (category:string) => {
+  const [filter, setFilter] = React.useState<null | String>('');
+  const [showArtistFilter, setShowArtistFilter] = React.useState(false);
+  const [selectedArtists, setSelectedArtists] = React.useState<string[]>([]);
+  const categories = [
+    'CD',
+    '티셔츠',
+    '목걸이',
+    '텀블러',
+    '핸드폰케이스',
+    '잠옷',
+    '머그컵',
+  ];
+  const artists = [
+    'YOASOBI',
+    '보아',
+    '트와이스',
+    '칸나',
+    '에스파',
+    '있지',
+    '아이즈원',
+  ];
+  const handleFilterTeb = (category: string) => {
     setFilter(category);
-  }
-  const filteredProduct = filter?ProducList.filter(product=>product.id===filter):ProducList;
-  const categories =['후드티','모자','티셔츠','잠옷','블라블라']
+  };
+  const handleArtistChange = (artist: string) => {
+    setSelectedArtists((prevArtists) =>
+      prevArtists.includes(artist)
+        ? prevArtists.filter((a) => a !== artist)
+        : [...prevArtists, artist],
+    );
+  };
+  const resetArtistFilter = () => {
+    setSelectedArtists([]);
+  };
+  const filteredProduct = ProducList.filter(
+    (product) =>
+      (selectedArtists.length === 0 ||
+        selectedArtists.includes(product.artist)) &&
+      (!filter || product.category === filter),
+  );
+
   return (
     <S.GoodsListContainer>
       <S.GoodsListSection1>
-        <S.AtistFilter>아티스트 선별용 </S.AtistFilter>
+        <S.AtistFilter isOpen={showArtistFilter}>
+          <S.AtistFilterWrapper>
+            <S.AtistFilterBtn
+              isOpen={!showArtistFilter}
+              onClick={() => setShowArtistFilter(!showArtistFilter)}
+            >
+              아티스트 필터
+            </S.AtistFilterBtn>
+            {showArtistFilter && (
+              <>
+                <S.AtistFilterContainer>
+                  {artists.map((artist) => (
+                    <S.AtistFilterArtist>
+                      <input
+                        type="checkbox"
+                        checked={selectedArtists.includes(artist)}
+                        onChange={() => handleArtistChange(artist)}
+                        id="artist"
+                      />
+                      <label htmlFor="artist">{artist}</label>
+                    </S.AtistFilterArtist>
+                  ))}
+                </S.AtistFilterContainer>
+                <S.AtistFilterReset onClick={resetArtistFilter}>
+                  초기화
+                </S.AtistFilterReset>
+              </>
+            )}
+          </S.AtistFilterWrapper>
+        </S.AtistFilter>
+
         <S.PageLocation>페이지 위치 알려주는곳 </S.PageLocation>
       </S.GoodsListSection1>
       <S.GoodsListSection2>
-        {/* TODO: 상품 태그 생기면 MAP 사용해서 코드 줄이기  */}
-    {
-      categories.map((category)=>(
-        <S.ProductsTab onClick={()=>handleFilterTeb(category)}>{category}</S.ProductsTab>
-      ))
-    }
-        
+        <S.GoodsListSection2Wrapper>
+          <S.ProductsTab key="all" onClick={() => setFilter(null)}>
+            전체보기
+          </S.ProductsTab>
+          {categories.map((category) => (
+            <S.ProductsTab
+              key={category}
+              onClick={() => handleFilterTeb(category)}
+            >
+              {category}
+            </S.ProductsTab>
+          ))}
+        </S.GoodsListSection2Wrapper>
       </S.GoodsListSection2>
       {/* TODO: 상품리스트 카드 */}
       <S.GoodsListSection3>
-        {
-          //상품 필터해서 0개면 상품없다고 말해줌
-filteredProduct?.length>0?(
-        filteredProduct?.map((product) => (
-          <S.ProductCard>
-            <S.ProductCardImg src={product.img} alt="상품이미지" />
-            <S.GoodsListCardSection1>
-              <S.GoodsListCardSection1_1>
-                <S.ProductCardInfo>{product.info}?</S.ProductCardInfo>
-                <S.ProductCardTitle>{product.title}</S.ProductCardTitle>
-              </S.GoodsListCardSection1_1>
-              <S.GoodsListCardSection1_2>
-                <S.ProductCardPrice>{product.price}</S.ProductCardPrice>
-                <S.ProductCardTeg src={product.teg} alt="이미지태그" />
-              </S.GoodsListCardSection1_2>
-            </S.GoodsListCardSection1>
-          </S.ProductCard>
-        ))
-  ):(<div>상품이 없습니다.</div>)
-        }
+        <S.GoodsListSection3Wrapper>
+          {
+            //상품 필터해서 0개면 상품없다고 말해줌
+            filteredProduct?.length > 0 ? (
+              filteredProduct?.map((product) => (
+                <S.ProductCard>
+                  <S.ProductCardImgBox>
+                    <S.ProductCardImg src={product.img} alt="상품이미지" />
+                  </S.ProductCardImgBox>
+                  <div>
+                    <S.GoodsListCardSection1>
+                      <S.GoodsListCardSection1_1>
+                        <S.ProductCardInfo>{product.info}?</S.ProductCardInfo>
+                        <S.ProductCardTitle>{product.title}</S.ProductCardTitle>
+                      </S.GoodsListCardSection1_1>
+                      <S.GoodsListCardSection1_2>
+                        <S.ProductCardPrice>{product.price}</S.ProductCardPrice>
+                        <S.ProductCardTeg src={product.teg} alt="이미지태그" />
+                      </S.GoodsListCardSection1_2>
+                    </S.GoodsListCardSection1>
+                  </div>
+                </S.ProductCard>
+              ))
+            ) : (
+              <div>상품이 없습니다.</div>
+            )
+          }
+        </S.GoodsListSection3Wrapper>
       </S.GoodsListSection3>
       <S.GoodsListSection4>1</S.GoodsListSection4>
     </S.GoodsListContainer>
--- src/styledComponent/styledGoodsList/StGoodsList.tsx
+++ src/styledComponent/styledGoodsList/StGoodsList.tsx
@@ -1,13 +1,21 @@
 import styled from 'styled-components';
-
+type AtistFilterProps = {
+  isOpen: boolean;
+  onClick?: () => void;
+};
 export const Div = styled.div``;
 
 export const GoodsListContainer = styled.div`
-margin-top: 7vh;
+  max-width: 2200px;
+
+  margin: auto;
+  margin-top: 7vh;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   padding: 20px;
+  position: relative;
 `;
 //아티스트 선별? // 페이지 위치 알려주는곳 ? 여기는 flex 써도 될듯?
 export const GoodsListSection1 = styled.section`
@@ -20,11 +28,61 @@ export const GoodsListSection1 = styled.section`
   padding: 20px;
 `;
 //아티스트 필터
-export const AtistFilter = styled.div``;
+export const AtistFilter = styled.button<AtistFilterProps>`
+  width: 250px;
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  height: 3vh;
+  transition: 0.1s;
+  color: ${(props) => (props.isOpen ? 'white' : 'black')};
+  background-color: ${(props) => (props.isOpen ? '#000000' : 'white')};
+`;
+export const AtistFilterBtn = styled.button<AtistFilterProps>`
+  width: 100%;
+  text-align: left;
+
+  /* background-color: ${(props) => (props.isOpen ? 'white' : '#000000')}; */
+`;
+export const AtistFilterWrapper = styled.div`
+  position: relative;
+  width: 250px;
+`;
+export const AtistFilterContainer = styled.div`
+  width: 250px;
+
+  background-color: white;
+  position: absolute;
+  top: -22px;
+  left: 260px;
+  border-radius: 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  background-color: #000000;
+  z-index: 1;
+`;
+export const AtistFilterArtist = styled.div`
+  margin-top: 5px;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+`;
+export const AtistFilterReset = styled.button`
+  position: absolute;
+  left: 150px;
+  border-radius: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`;
 //페이지 위치 알려주는곳
 export const PageLocation = styled.div``;
 //상품종류 필터 ex) 후드티 모자 티셔츠 잠옷 등
 export const GoodsListSection2 = styled.section`
+  width: 100%;
+`;
+export const GoodsListSection2Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
@@ -50,6 +108,9 @@ export const ProductsTab = styled.div`
 `;
 //상품리스트
 export const GoodsListSection3 = styled.section`
+  width: 100%;
+`;
+export const GoodsListSection3Wrapper = styled.div`
   margin-top: 20px;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -60,16 +121,30 @@ export const GoodsListSection3 = styled.section`
 
 export const ProductCard = styled.div`
   width: 250px;
-  height: 350px;
+  height: 370px;
+  padding: 5px;
   margin: auto;
   border: 1px solid black;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const ProductCardImg = styled.img`
+  flex: 1;
   width: 100%;
+  max-height: 230px;
+  height: 100%;
   border-radius: 30px;
+  margin: auto;
+`;
+export const ProductCardImgBox = styled.div`
+  width: 100%;
+
+  border-radius: 30px;
+  background-color: #f5f5f5;
 `;
 export const ProductCardInfo = styled.span`
+  flex: 2;
   width: 100%;
   height: 100%;
   color: #ffbd76;
